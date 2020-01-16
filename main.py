b83c4ccfb18aa210bee46035e175d18bce90ded4
Minish144 <varlamow.col@yahoo.com>
2020-01-16 22:03:33 +0300

    test

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ from datetime import timezone
 from datetime import datetime
 import os
 import urllib.request
+import random
 
 with open("vk_data.json", "r") as vk_data_json_file:
         vk_data = json.load(vk_data_json_file)
 
+class vk_class:
+    login = vk_data["login"]
+    password = vk_data["password"]
+    access_token = vk_data["access_token"]
+    owner_id = vk_data["owner_id"]
+    chat_id = vk_data["chat_id"]
+    session = VkApi(login=login,
+                    password=password,
+                    token=access_token)
+
 def main():
-    pass
+    vk = vk_class.session.get_api()
+    vk.messages.send(chat_id = vk_class.chat_id, message='test', random_id = random.random())
 
 if __name__ == "__main__":
     main()
