246fc185041e27cd52e6c315e7abc89b2619ac31
Minish144 <varlamow.col@yahoo.com>
2020-01-17 01:47:03 +0300

    final result

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def get_messages_total():
 def get_messages_and_count(messages, messages_amount_per_week, messages_amount_total):
     time_current = time.time()
     time_week_ago = time_current-604800
-    #for i in range(messages_amount_total//200):
-    for i in range(15):
+    for i in range(messages_amount_total//200):
+    #for i in range(15):
         items = vk.messages.getHistory(peer_id=peer_id, count=200, offset=i*200)['items']
         for k in range(200):
             messages[0] += items[k]['text'] + ' '
@@ -76,13 +76,15 @@ def main():
     get_messages_and_count(messages, messages_amount_per_week, messages_amount_total)
     messages = messages[0]; messages_amount_per_week = messages_amount_per_week[0]
     print('сообщений в конфе за 7 дней:', messages_amount_per_week)
-    #vk.messages.send(peer_id=peer_id, message=f'Сообщений в конфе за 7 дней: {messages_amount_per_week}', random_id = random.random())
     messages = lemmatize(messages)
     word_list = message_format_and_split(messages)
     word_freq_dict = most_freq_word_in_a_week(word_list)
-    word_freq_dict = list(filter(lambda x : len(x["word"]) > 5, word_freq_dict))
-    word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount']) 
+    word_freq_dict = list(filter(lambda x : len(x["word"]) > 3, word_freq_dict))
+    word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount'], reverse=True) 
     print(word_freq_dict)
+
+    vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе за 7 дней: {messages_amount_per_week}\n Пять самых популярных слов за 7 дней: \n {word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n {word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n {word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n {word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n {word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
+
 
 if __name__ == "__main__":
     main()
