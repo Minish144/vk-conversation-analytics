8a8a048b2a82594f63c678c9622e60d2ab4041f2
Minish144 <varlamow.col@yahoo.com>
2020-01-17 14:33:31 +0300

    fixed messages per week counter

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import re
 from pymystem3 import Mystem
 
 with open("vk_data.json", "r") as vk_data_json_file:
-        vk_data = json.load(vk_data_json_file)
+    vk_data = json.load(vk_data_json_file)
 
 # # # # # # # # # # # # # # # # # # # # # # #
                                             #
@@ -31,14 +31,19 @@ def get_messages_total():
 def get_messages_and_count(messages, messages_amount_per_week, messages_amount_total):
     time_current = time.time()
     time_week_ago = time_current-604800
-    time_day_ago = time_current-86400
+    #time_day_ago = time_current-86400
+    exit_flag = False
     for i in range(messages_amount_total//200):
+        messages_amount_per_week[0]
     #for i in range(15):
         items = vk.messages.getHistory(peer_id=peer_id, count=200, offset=i*200)['items']
         for k in range(200):
+            if items[k]['date'] <= time_week_ago:
+                exit_flag = True
+                break
             messages[0] += items[k]['text'] + ' '
-        messages_amount_per_week[0] += k
-        if items[k]['date'] <= time_week_ago:
+            messages_amount_per_week[0] += 1
+        if exit_flag == True:
             break
 
 def lemmatize(messages):
@@ -65,6 +70,8 @@ def most_freq_word_in_a_week(word_list):
         for secondary_word in word_list:
             if primary_word == secondary_word:
                 count+=1
+        if primary_word == 'люда':
+            primary_word = 'люда (не пиши)'
         word_freq_dict.append({"word":primary_word,
                                "amount":count})
     return word_freq_dict
@@ -80,8 +87,8 @@ def main():
     word_freq_dict = most_freq_word_in_a_week(word_list)
     word_freq_dict = list(filter(lambda x : len(x["word"]) > 3, word_freq_dict))
     word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount'], reverse=True) 
-    vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\nПять самых популярных слов за 7 дней: \n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
-    print(f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\nПять самых популярных слов за 7 дней: \n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
+    vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
+    print(f'Сообщений в конфе всего - {messages_amount_total}\nСообщений в конфе за 7 дней - {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней:\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
 
 if __name__ == "__main__":
     main()
