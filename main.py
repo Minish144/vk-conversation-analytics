e7f6b5220e7bd2a5e94eafe97da27e8b4353a7dd
Minish144 <varlamow.col@yahoo.com>
2020-01-17 14:57:54 +0300

    minor fixes

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main():
     word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount'], reverse=True) 
     vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
     print(f'Сообщений в конфе всего - {messages_amount_total}\nСообщений в конфе за 7 дней - {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней:\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
-
+    #print(word_freq_dict[:20])
 if __name__ == "__main__":
     main()
 
