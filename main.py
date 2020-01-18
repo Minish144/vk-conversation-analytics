9bb6f06b12e0035104f580e44af78bac0b6271fb
Minish144 <varlamow.col@yahoo.com>
2020-01-19 02:50:12 +0300

    huge updates!

--- main.py
+++ main.py
@@ -64,32 +64,21 @@ def lemmatize(messages):
     return ''.join(lemmas)
 
 def message_format_and_split(messages):
-    s = messages
     reg = re.compile('[^a-zA-Zа-яА-Я ]')
-    reg = reg.sub('', s)
+    reg = reg.sub('', messages)
     return reg.split()
-    
-def remove_empty_element(word_list):
-    for x in word_list:
-        if x == '':
-            word_list.remove('')
-
-def most_freq_word_in_a_week(word_list):
+ 
+def most_freq_word(word_list):
     word_freq_dict = []
     count = 0
     for primary_word in set(word_list):
-        count = 0
-        for secondary_word in word_list:
-            if primary_word == secondary_word:
-                count+=1
-        if primary_word == 'люда':
-            primary_word = 'люда (не пиши)'
-        word_freq_dict.append({"word":primary_word,
-                               "amount":count})
+        count = word_list.count(primary_word)
+        word_freq_dict.append({"word": primary_word,
+                               "amount": count})
     return word_freq_dict
 
 def get_last_message():
-    return vk.messages.getHistory(peer_id = peer_id, count = 5)['items'][0]['text']
+    return vk.messages.getHistory(peer_id = peer_id, count = 1)['items'][0]['text']
 
 def stats_per_week():
     try:
@@ -101,16 +90,16 @@ def stats_per_week():
         get_messages_per_week_and_count(messages, messages_amount_per_week, messages_amount_total)
         messages = messages[0]; messages_amount_per_week = messages_amount_per_week[0]
         word_list = message_format_and_split(messages)
-        word_freq_dict = most_freq_word_in_a_week(word_list)
+        word_freq_dict = most_freq_word(word_list)
         word_freq_dict = list(filter(lambda x : len(x["word"]) > 3, word_freq_dict))
         word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount'], reverse=True) 
-        vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
+        vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_week}\n-------------------------------------\nПять самых популярных слов за 7 дней\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
         print(f'Сообщений в конфе всего - {messages_amount_total}\nСообщений в конфе за 7 дней - {messages_amount_per_week}\n-----------------------------------------\nПять самых популярных слов за 7 дней:\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз') 
     except:
         try:
-            vk.messages.send(peer_id=peer_id, random_id = random.random(), message='привет бот сдох')
+            vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Бот сломался!\n{Exception}')
         except:
-            print(Exception)
+            print(f'Бот сломался!\n{Exception}')
 
 def stats_per_day():
     try:
@@ -122,34 +111,24 @@ def stats_per_day():
         get_messages_per_day_an_count(messages, messages_amount_per_day, messages_amount_total)
         messages = messages[0]; messages_amount_per_day = messages_amount_per_day[0]
         word_list = message_format_and_split(messages)
-        word_freq_dict = most_freq_word_in_a_week(word_list)
+        word_freq_dict = most_freq_word(word_list)
         word_freq_dict = list(filter(lambda x : len(x["word"]) > 3, word_freq_dict))
         word_freq_dict = sorted(word_freq_dict, key=lambda k: k['amount'], reverse=True) 
-        vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за 7 дней: {messages_amount_per_day}\n-----------------------------------------\nПять самых популярных слов за 7 дней\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
-        print(f'Сообщений в конфе всего - {messages_amount_total}\nСообщений в конфе за 7 дней - {messages_amount_per_day}\n-----------------------------------------\nПять самых популярных слов за 7 дней:\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз') 
+        vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Сообщений в конфе всего: {messages_amount_total}\nСообщений в конфе за день: {messages_amount_per_day}\n-------------------------------------\nПять самых популярных слов за день\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз')
+        print(f'Сообщений в конфе всего - {messages_amount_total}\nСообщений в конфе за день - {messages_amount_per_day}\n-----------------------------------------\nПять самых популярных слов за день:\n{word_freq_dict[0]["word"]} - {word_freq_dict[0]["amount"]} раз \n{word_freq_dict[1]["word"]} - {word_freq_dict[1]["amount"]} раз \n{word_freq_dict[2]["word"]} - {word_freq_dict[2]["amount"]} раз \n{word_freq_dict[3]["word"]} - {word_freq_dict[3]["amount"]} раз \n{word_freq_dict[4]["word"]} - {word_freq_dict[4]["amount"]} раз') 
     except:
         try:
-            vk.messages.send(peer_id=peer_id, random_id = random.random(), message='привет бот сдох')
+            vk.messages.send(peer_id=peer_id, random_id = random.random(), message=f'Бот сломался!\n{Exception}')
         except:
-            print(Exception)
-
-def kolb_cocu():
-    try:
-        vk.messages.send(peer_id=peer_id, random_id = random.random(), message='сам соси')
-    except:
-        try:
-            vk.messages.send(peer_id=peer_id, random_id = random.random(), message='привет бот сдох')
-        except:
-            print(Exception)
+            print(f'Бот сломался!\n{Exception}')
 
 def main():
     while (1):
-        if get_last_message() == 'колб стата':
+        last_message = get_last_message()
+        if last_message == 'статистика неделя':
             stats_per_week()
-        if get_last_message() == 'колб сегодня':
+        elif last_message == 'статистика сегодня':
             stats_per_day()
-        if get_last_message().find('колб соси') != -1:
-            kolb_cocu()
 
 if __name__ == "__main__":
     main()
